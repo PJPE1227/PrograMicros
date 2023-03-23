b5af6cd9a10a152acf4c403861912e6cfb16ff30
PJPE1227 <124507174+PJPE1227@users.noreply.github.com>
2023-03-23 01:39:45 -0600

    Create ejercicioUno.c

---
+++ ExamenCorto3/ejercicioUno.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Pablo Peña";
+    int carnet = 21210;
+    printf("Mi nombre es name %sy mi carnet es carnet %d\n", name, carnet);
+
+    return 0;
+}
