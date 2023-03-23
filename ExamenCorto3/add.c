9a807cfe54156f8bae5a3c35ad9036419bd89146
PJPE1227 <124507174+PJPE1227@users.noreply.github.com>
2023-03-23 01:43:31 -0600

    Create add.c

---
+++ ExamenCorto3/add.c
@@ -0,0 +1,26 @@
+#include "add.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+int mul(int a, int b)
+{
+    return a*b;
+}
+
+int dev(int a, int b)
+{
+    return a/b;
+}
+
+double pi()
+{
+    return 3.14159;
+}
